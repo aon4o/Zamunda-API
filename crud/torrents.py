7a122cf97608ccbda1526745dbad5225543e6ad5
Yoan Laptop <yoanivanov9@gmail.com>
2023-11-23 18:43:21 +0200

    torrents crud tested

--- crud/torrents.py
+++ crud/torrents.py
@@ -3,12 +3,13 @@ from sqlalchemy.orm import Session
 from models import torrent as torrent_model
 from schemas import torrent as torrent_schema
 
+from datetime import datetime
+
 def list_all(db: Session, skip: int = 0, limit: int = 100):
     return (db.query(torrent_model.Torrent)
             .offset(skip)
             .limit(limit)
             .all())
-
 
 def get(db: Session, torrent_id: int):
     return (db.query(torrent_model.Torrent)
@@ -16,7 +17,13 @@ def get(db: Session, torrent_id: int):
 
 
 def create(db: Session, torrent: torrent_schema.TorrentCreate):
-    db_torrent = torrent_model.Torrent(name=torrent.name, description=torrent.description)
+    db_torrent = torrent_model.Torrent( user_id=torrent.user_id,
+                                        name=torrent.name, 
+                                        description=torrent.description,
+                                        file = torrent.file,
+                                        times_downloaded = torrent.times_downloaded,
+                                        sub_category_id = torrent.sub_category_id,
+                                    )
     db.add(db_torrent)
     db.commit()
     db.refresh(db_torrent)
@@ -25,9 +32,15 @@ def create(db: Session, torrent: torrent_schema.TorrentCreate):
 
 def update(db: Session, torrent_id: int, torrent: torrent_schema.TorrentCreate):
     db_torrent = get(db, torrent_id)
+      
     db_torrent.name = torrent.name
     db_torrent.description = torrent.description
+    # db_torrent.image = torrent.image
+    db_torrent.file = torrent.file
+    db_torrent.times_downloaded = torrent.times_downloaded
+    db_torrent.sub_category_id = torrent.sub_category_id
     db_torrent.updated_at = datetime.now()
+    
     db.commit()
     db.refresh(db_torrent)
     return db_torrent
