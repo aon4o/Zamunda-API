e73614765761e42d4b09d78ba220b7b0c420ac91
Yoan Laptop <yoanivanov9@gmail.com>
2023-11-23 17:58:37 +0200

    torrents crud

--- crud/torrents.py
+++ crud/torrents.py
@@ -1,16 +1,40 @@
 from sqlalchemy.orm import Session
 
-from models import torrent as models
-from schemas import torrent as schemas
+from models import torrent as torrent_model
+from schemas import torrent as torrent_schema
+
+def list_all(db: Session, skip: int = 0, limit: int = 100):
+    return (db.query(torrent_model.Torrent)
+            .offset(skip)
+            .limit(limit)
+            .all())
 
 
-def get_torrents(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Torrent).offset(skip).limit(limit).all()
+def get(db: Session, torrent_id: int):
+    return (db.query(torrent_model.Torrent)
+            .get({'id': torrent_id}))
 
 
-def create_user_torrent(db: Session, torrent: schemas.TorrentCreate, user_id: int):
-    db_torrent = models.Torrent(**torrent.dict(), owner_id=user_id)
+def create(db: Session, torrent: torrent_schema.TorrentCreate):
+    db_torrent = torrent_model.Torrent(name=torrent.name, description=torrent.description)
     db.add(db_torrent)
     db.commit()
     db.refresh(db_torrent)
     return db_torrent
+
+
+def update(db: Session, torrent_id: int, torrent: torrent_schema.TorrentCreate):
+    db_torrent = get(db, torrent_id)
+    db_torrent.name = torrent.name
+    db_torrent.description = torrent.description
+    db_torrent.updated_at = datetime.now()
+    db.commit()
+    db.refresh(db_torrent)
+    return db_torrent
+
+
+def delete(db: Session, torrent_id: int):
+    db_torrent = get(db, torrent_id)
+    db.delete(db_torrent)
+    db.commit()
+    return None
